9a0673e5e5b1a7864bb1ef2a230f418ad8b0970d
synthetixapp17 <synthetixapp17@gmail.com>
2026-08-29 16:54:50 -0400

    Descripción del cambio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3420906312" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1755888436" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
