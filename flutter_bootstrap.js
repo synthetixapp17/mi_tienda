687a6dc78b45ccc0125b825a1ed4a79ed1f50861
synthetixapp17 <synthetixapp17@gmail.com>
2026-09-03 18:19:25 -0400

    Actualizacion completa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,6 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c0010","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c0010","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "3129724407" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  }
+});
